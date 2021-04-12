c1f77e891c784bdcf7e0809b2a922c25dc72b556
Art-Orl <orlyuta95@mail.ru>
2021-04-12 22:09:45 +0300

    feat: add 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+  let str1 = '';
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else if (count > 1) {
+      str1 += count.toString() + str[i];
+      count = 1;
+    } else {
+      str1 += str[i];
+    }
+  }
+  return str1;
 }
 
 module.exports = encodeLine;
